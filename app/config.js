c551985372cbf858d3a328664979409168775045
Tom Simcox <tomsimcox.digitaldwp@gmail.com>
2016-08-18 15:29:16 +0100

    minor content change

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Your dwp benefits',
+  serviceName: 'Your DWP benefits',
 
   // Default port that prototype runs on
   port: '3000',
